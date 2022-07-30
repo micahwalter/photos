4c5db0e2c6568d99a565ba549416ff3d8fd44dac
Micah Walter <micah@hey.com>
2022-07-30 19:13:48 +0000

    adding metadata

--- backend/src/photoProcessing/photoProcessing.js
+++ backend/src/photoProcessing/photoProcessing.js
@@ -1,7 +1,9 @@
 const AWS = require('aws-sdk');
 const dynamo = new AWS.DynamoDB.DocumentClient();
+const s3 = new AWS.S3();
 
 const axios = require('axios').default;
+const sharp = require('sharp');
 
 exports.handler = async (event) => {
     
@@ -10,6 +12,7 @@ exports.handler = async (event) => {
     // first we need to look up the image by its S3 key to see if it already exists
 
     const objectKey = event.detail.object.key;
+    const srcBucket = event.detail.bucket.name;
     
     var params = {
         TableName: 'photos',
@@ -37,19 +40,145 @@ exports.handler = async (event) => {
         brooklynInt = data.Items[0].id
     }
     
+    // download the image from s3
+    try {
+        const params = {
+            Bucket: srcBucket,
+            Key: objectKey
+        };
+        var origimage = await s3.getObject(params).promise();
+
+    } catch (error) {
+        console.log(error);
+        return;
+    }
+    
     // next use sharp to do image processing
+    
+    // create a thumbnail
+    var width  = 300;
+    
+    try {
+      var buffer = await sharp(origimage.Body).resize(width).jpeg().toBuffer();
+
+    } catch (error) {
+        console.log(error);
+        return;
+    }
+    
+    // save result on destination bucket
+    const baseName = objectKey.replace(/\.[^/.]+$/, "")
+    
+    const thumbnailKey = baseName + "/" + baseName + "_t.jpg";
+    
+    try {
+      const destparams = {
+          Bucket: process.env.targetBucket,
+          Key: thumbnailKey,
+          Body: buffer,
+          ContentType: "image"
+      };
+
+      const putResult = await s3.putObject(destparams).promise();
+
+    } catch (error) {
+        console.log(error);
+        return;
+    }
+
 
     // create derivitive sizes and store in target bucket
 
-    // with naming conventions like
-    // <Brooklyn Int>/<Brooklyn Int>_<size>.jpg
-
-    // copy and rename the original file ..
+    // medium
+    width  = 1200;
     
+    try {
+      var buffer = await sharp(origimage.Body).resize(width).jpeg().toBuffer();
+
+    } catch (error) {
+        console.log(error);
+        return;
+    }
+    
+    // save result on destination bucket    
+    const mediumKey = baseName + "/" + baseName + "_m.jpg";
+    
+    try {
+      const destparams = {
+          Bucket: process.env.targetBucket,
+          Key: mediumKey,
+          Body: buffer,
+          ContentType: "image"
+      };
+
+      const putResult = await s3.putObject(destparams).promise();
+
+    } catch (error) {
+        console.log(error);
+        return;
+    }
+
+    // large
+    width  = 2400;
+    
+    try {
+      var buffer = await sharp(origimage.Body).resize(width).jpeg().toBuffer();
+
+    } catch (error) {
+        console.log(error);
+        return;
+    }
+    
+    // save result on destination bucket    
+    const largeKey = baseName + "/" + baseName + "_l.jpg";
+    
+    try {
+      const destparams = {
+          Bucket: process.env.targetBucket,
+          Key: largeKey,
+          Body: buffer,
+          ContentType: "image"
+      };
+
+      const putResult = await s3.putObject(destparams).promise();
+
+    } catch (error) {
+        console.log(error);
+        return;
+    }    
+
+    // original ..
+    
+    try {
+        var buffer = await sharp(origimage.Body).toFormat('jpeg').toBuffer();
+  
+    } catch (error) {
+        console.log(error);
+        return;
+    }
+
+    const originalKey = baseName + "/" + baseName + "_o.jpg";
+    
+    try {
+      const destparams = {
+          Bucket: process.env.targetBucket,
+          Key: originalKey,
+          Body: buffer,
+          ContentType: "image"
+      };
+
+      const putResult = await s3.putObject(destparams).promise();
+
+    } catch (error) {
+        console.log(error);
+        return;
+    }
 
     // extract metadata
-
+    const metadata = await sharp(origimage.Body).metadata();
+    
     // extract stats
+    const stats = await sharp(origimage.Body).stats();
 
     // store metadata and stats in DynamoDB with Brooklyn Int as id
     // if a new record use put, otherwise use update
@@ -60,7 +189,15 @@ exports.handler = async (event) => {
                 'id': brooklynInt,
                 'originalFileObjectKey': objectKey,
                 'createdAt': Math.floor(new Date().getTime() / 1000),
-                'updatedAt': null
+                'updatedAt': null,
+                'images': {
+                    'o': originalKey,
+                    'l': largeKey,
+                    'm': mediumKey,
+                    't': thumbnailKey,
+                },
+                'metadata': metadata,
+                'stats': stats
             }
         };
       
